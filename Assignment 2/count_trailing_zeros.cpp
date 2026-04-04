16c6d9ef7aae0fec0abfe67df00c896d58ccd4fc
CODEFORBLIND <chelsimanishjain@gmail.com>
2026-04-04 23:58:28 +0530

    initial commit

---
+++ Assignment 2/count_trailing_zeros.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int countTrailingZeroes(int A) {
+
+    if (A == 0) {
+        return 0;
+    }
+
+    int count = 0;
+
+    while ((A & 1) == 0) {
+        count++;      
+        A = A >> 1;   
+    }
+
+    return count;
+}
+
+int main() {
+
+    int A = 18;
+
+    int result = countTrailingZeroes(A);
+
+    cout << result << endl;
+
+    return 0;
+}
