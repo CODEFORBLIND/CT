886f31a7312c219a8a8a66b3beb68e9c03516069
CODEFORBLIND <chelsimanishjain@gmail.com>
2026-04-08 23:53:18 +0530

    initial commit

--- Examples/dutchsort.cpp
+++ Examples/dutchsort.cpp
@@ -2,44 +2,29 @@
 #include <vector>
 using namespace std;
 
-void sort012(vector<int> &arr) {
-    int n = arr.size();
-    
-    // initialize three pointers:
-    // lo: boundary for 0s
-    // mid: current element being checked
-    // hi: boundary for 2s
-    int lo = 0;
-    int hi = n - 1;
-    int mid = 0;
-    
-    // process elements until mid crosses hi
-    while (mid <= hi) {
-        if (arr[mid] == 0) {
-            
-            // current is 0: swap with lo and move both 
-            // pointers forward
-            swap(arr[lo++], arr[mid++]);
-        }
-        else if (arr[mid] == 1) {
-            // current is 1: it's already in correct position
-            mid++;
-        }
-        else {
-            // current is 2: swap with hi and move hi backward
-            // do not increment mid, as swapped value needs
-            // to be re-checked
-            swap(arr[mid], arr[hi--]);
+void sort012(int arr[], int n) {
+    int first = 0;
+    int last = n-1;
+    int counter = 0;
+    while(counter <= last){
+        if(arr[counter] == 0){
+            swap(arr[first], arr[counter]);
+            first++;
+            counter++;
+        } else if(arr[counter] == 2){
+            swap(arr[counter], arr[last]);
+            last--;
+        } else{
+            counter++;
         }
     }
 }
 
 int main() {
-    vector<int> arr = { 0, 1, 2, 0, 1, 2 };
-    int n = arr.size();
+    int arr[] = { 0, 1, 2, 0, 1, 2 };
+    int n = sizeof(arr)/ sizeof(int);
     
-    // sort the array in-place
-    sort012(arr); 
+    sort012(arr, n); 
 
     for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
