d6f944fbec0d760c674263bce3e79e619624c4b4
CODEFORBLIND <chelsimanishjain@gmail.com>
2026-04-11 11:10:21 +0530

    initial commit

---
+++ Assignment 4/Assign4_1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+bool anagram(std::string a, std::string b)
+{
+    std::map<char, int> map_a;
+    std::map<char, int> map_b;
+
+    for (int i = 0; i < a.length(); i++)
+    {
+        if (map_a.count(a[i]) == 0)
+        {
+            map_a[a[i]] = 1;
+        }
+        else
+        {
+            map_a[a[i]]++;
+        }
+    };
+
+    for (int i = 0; i < b.length(); i++)
+    {
+        if (map_b.count(b[i]) == 0)
+        {
+            map_b[b[i]] = 1;
+        }
+        else
+        {
+            map_b[b[i]]++;
+        }
+    };
+
+    return map_a == map_b;
+};
+
+int main()
+{
+
+    bool result = anagram("greate", "regate");
+    if (result)
+    {
+        std::cout << "Anagram!" << std::endl;
+    }
+    else
+    {
+        std::cout << "Not an Anagram!" << std::endl;
+    }
+    return 0;
+}
---
+++ Examples/trainproblem.cpp
@@ -0,0 +1,12 @@
+// Arrival time
+// departure time
+// min platforms that no train waits
+// platforms = 1 to 10
+// time = 0 to 2359
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    
+return 0;
+}
