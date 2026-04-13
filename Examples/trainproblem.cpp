e1abdd9a71835233f2bfdfe9d05fafab881008de
CODEFORBLIND <chelsimanishjain@gmail.com>
2026-04-13 09:44:39 +0530

    initial commit

--- Examples/trainproblem.cpp
+++ Examples/trainproblem.cpp
@@ -1,12 +1,31 @@
-// Arrival time
-// departure time
+// Arrival times
+// departure times
 // min platforms that no train waits
 // platforms = 1 to 10
-// time = 0 to 2359
+// time = 0000 to 2359
 
 #include<bits/stdc++.h>
 using namespace std;
+
+int minplatforms(int arr[], int dep[], int size){
+    int plt = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if(arr[i] < dep[i]){
+            if(arr[i+1] >= dep[i]){
+            } else if(arr[i+1] < dep[i]){
+                plt++;
+            } 
+        }
+    }
+    return plt;
+}
+
 int main(){
-    
+    int arr[] = {200, 259, 339, 359, 559};
+    int dep[] = {300, 340, 400, 600, 715};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    int number = minplatforms(arr, dep, size);
+    cout << "Min number of platforms: " << number << endl;
 return 0;
 }
