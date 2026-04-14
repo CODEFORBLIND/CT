145e04398797aca74beb47382e7a606b351fd599
CODEFORBLIND <chelsimanishjain@gmail.com>
2026-04-14 09:46:37 +0530

    initial commit

--- Assignment 5/Assign5_1.cpp
+++ Assignment 5/Assign5_1.cpp
@@ -1,87 +1,52 @@
-
-    // 1 -> 10
-    // 2 -> 12
-    // 3 -> 15
-    // 4 -> 14
-    
-    // signal      Waiting Time
-    // 1              1 -> 2 -> 3          
-    // 2              0
-    // 3              1  
-    // 4              1 -> 2
-    // total -> 6 (minimum)
-
-
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-typedef struct{
-    int signal_no;
-    int num_of_vehicles;
-    int waiting_time;
-} tsv;
-
-
-void waitingTime(tsv signal[], int num){
-    int min = signal[0].num_of_vehicles;
-    int sum = 0;
-    int index = 0;
-    int k = 0;
-    for (int i = 0; i < num; i++)
-    {
-        sum += signal[index].num_of_vehicles;
+bool is_clear(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+    if (arr[i] == 0) {
+        continue;
+    } else {
+        return false;
     }
-    
-    while (sum != 0)
-    {
-        for (int i = 1; i < num; i++)
-        {
-            if(signal[i].num_of_vehicles < min){
-                min = signal[i].num_of_vehicles;
-                index = i;
-            }
-        }
-        // int x = signal[index].num_of_vehicles * 0.75;
-        // int empty_val = (x == (int)x) ? (int)x : (int)x + 1;
-        int empty_val = signal[index].num_of_vehicles;
-        signal[index].num_of_vehicles -= empty_val;
-        for (int i = 0; i < num; i++)
-        {
-            if(signal[i].waiting_time == 0){
-                continue;
-            }
-            signal[i].waiting_time += empty_val;
-        }
-        sum -= empty_val;
     }
-    
+    return true;
 }
 
-int main(){
-    int num;
-    cout << "Enter the number of signals: " ;
-    cin >> num;
-    cout << "Enter the number of vehicles for each signal: " << endl;
-    tsv signal[num];
-    for (int i = 0; i < num; i++)
-    {
-        cin >> signal[i].num_of_vehicles;
-        signal[i].waiting_time = 0;
-        signal[i].signal_no = i+1;
+int get_max(int arr[], int n) {
+    int m = 0;
+    for (int i = 0; i < n; i++) {
+    if (arr[i] > arr[m]) {
+        m = i;
     }
-    for (int i = 0; i < num; i++)
-    {
-        cout  << "Signal Number: " << signal[i].signal_no << " -> "<< signal[i].num_of_vehicles << " " << "Waiting time : "<< signal[i].waiting_time << endl;
-        
+    }
+    return m;
+}
+
+int main() {
+    int step = 0;
+    int signals[] = {4, 2, 6, 4, 2, 6, 9, 10, 2, 4};
+    int n = 10;
+    int k = 1; // 1 to 10 rotate
+    while (!is_clear(signals, n)) {
+    if (k > 10) {
+        k = 1;
+    }
+    if (signals[get_max(signals, 10)] >= k) {
+        signals[get_max(signals, 10)] = signals[get_max(signals, 10)] - k;
+    }
+    k++;
+    step++;
+    }
+
+    cout << "All signals are cleared!" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << signals[i] << " ";
     }
     cout << endl;
-    waitingTime(signal, num);
-    int twt = 0;
-    for (int i = 0; i < num; i++)
-    {
-        twt += signal[i].waiting_time;
-    }
-    cout << "Total waiting time: " << twt << endl;
 
-return 0;
+    cout << "Minimum Waiting Time: " << step << endl;
+
+    return 0;
 }
+
